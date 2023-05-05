84811fae837113adaf18d65310e0d306470bb663
luccalkm <lucca.kleinsorgen@gmail.com>
2023-05-05 09:10:29 -0300

    Mediator Introduced & First Query Handler

--- API/Program.cs
+++ API/Program.cs
@@ -1,18 +1,24 @@
+using Application.Activities;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Connect to database e.g. option.useMysql(...)
 builder.Services.AddDbContext<DataContext>(option => 
 {
     option.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+// Add CORS policy to accept any method from the Front-End
 builder.Services.AddCors(option => {
     option.AddPolicy("CorsPolicy", policy => {
         policy.AllowAnyMethod()
@@ -20,6 +26,10 @@ builder.Services.AddCors(option => {
               .WithOrigins("http://localhost:3000");
     });
 });
+
+// Setting up the standard place do lookup all handlers and register them 
+// Put a handler so it knows where its located and where to look
+builder.Services.AddMediatR(typeof(MultipleActivities.Handler));
 
 var app = builder.Build();
 
@@ -31,6 +41,8 @@ if (app.Environment.IsDevelopment())
 }
 
 // app.UseHttpsRedirection();
+
+// Use the CORS Policy created (should be before Authorization!!!)
 app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
@@ -40,14 +52,19 @@ app.MapControllers();
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 
+
+// Create the database service
 try
 {
     var context = services.GetRequiredService<DataContext>();
+    // Migrate transactions and execute them if needed
     await context.Database.MigrateAsync();
+    // Get seed data into context var, referencing the DataContext, therefore, the database.
     await Seed.SeedData(context);
 }
 catch (Exception ex)
 {
+    // Logging error and etc.
     var logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex, "An error occured during migration.");
 }
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import { Button, Header, List } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -8,19 +9,18 @@ function App() {
   useEffect(() => {
     axios.get('http://localhost:5000/api/activities')
     .then(res => {
-      console.log(res.data);
       setActivities(res.data);
     })
   },[])
 
   return (
-    <div className="App">
-      <h1>Activities</h1>
-      <ul>
+    <div>
+      <Header as='h2' icon='users' content='Activities' />
+      <List>
         {activities.map((activity: any) => {
           return <li key={activity.id}>{activity.title}</li>
         })}
-      </ul>
+      </List>
     </div>
   );
 }
