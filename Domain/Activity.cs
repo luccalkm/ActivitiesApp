4c17edafdfc213fa19b75906f7d5428dcdbe9e1c
lkleinsorgen <lucca.kleinsorgen@gmail.com>
2023-05-15 14:03:08 -0300

    Adding relationship table (Activity-AppUser)

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -2,20 +2,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Domain
 {
-    public class Activity
-    {
-        public Guid Id { get; set; }
-        [Required]
-        public string Title { get; set; }
-        [Required]
-        public DateTime Date { get; set; }
-        [Required]
-        public string Description { get; set; }
-        [Required]
-        public string Category { get; set; }
-        [Required]
-        public string City { get; set; }
-        [Required]
-        public string Venue { get; set; }
-    }
+	public class Activity
+	{
+		public Guid Id { get; set; }
+		[Required]
+		public string Title { get; set; }
+		[Required]
+		public DateTime Date { get; set; }
+		[Required]
+		public string Description { get; set; }
+		[Required]
+		public string Category { get; set; }
+		[Required]
+		public string City { get; set; }
+		[Required]
+		public string Venue { get; set; }
+		public ICollection<ActivityAttendee> Attendees { get; set; }
+	}
 }
--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Domain
 {
-    public class AppUser : IdentityUser
-    {
-        public string DisplayName { get; set; }   
-        public string Bio { get; set; }
-    }
+	public class AppUser : IdentityUser
+	{
+		public string DisplayName { get; set; }
+		public string Bio { get; set; }
+		public ICollection<ActivityAttendee> Activities { get; set; }
+
+	}
 }
